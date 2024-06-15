2eee4ec69a63a96c71fea625659c32926f5ae9bf
Mark de Kraker <m.dekraker@floow.nl>
2024-06-15 21:12:12 +0200

    add vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { mediapipe } from 'vite-plugin-mediapipe';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mediapipe()],
 })
